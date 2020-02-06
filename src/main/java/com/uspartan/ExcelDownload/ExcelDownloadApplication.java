09bf0085fae94d642332ce321302b8531c051d89
Ullas T V <ullassharma29@gmail.com>
2020-02-06 19:46:14 +0530

    Add files via upload

---
+++ src/main/java/com/uspartan/ExcelDownload/ExcelDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.uspartan.ExcelDownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uspartan/ExcelDownload/ExcelDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uspartan.ExcelDownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
